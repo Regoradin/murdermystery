803a89355056b47b48cfe0d9ed7c289f35b11c07
Regoradin <oliver@theabates.com>
2020-01-30 01:08:01 -0800

    Montage object appears when off screen

---
+++ Unity/MurderMystery/Assets/Scripts/Montage.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Montage : MonoBehaviour
+{
+    private Renderer rend;
+    
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        Disappear();
+        if (!IsVisibleFromMainCam(rend)){
+            AttractAttention();
+        }
+        StartCoroutine(AppearWhenOffScreen());
+    }
+
+    void Update(){
+        Debug.Log("Visible: " + IsVisibleFromMainCam(rend));
+    }
+
+    private IEnumerator AppearWhenOffScreen(){
+        while (IsVisibleFromMainCam(rend)){
+            yield return null;
+        }
+        Appear();
+    }
+
+    private void AttractAttention(){
+        //Something to activate sound/lights will eventually go here
+        Debug.Log("Look at " + name + "!");
+    }
+
+    private void Disappear(){
+        rend.enabled = false;
+    }
+
+    private void Appear(){
+        rend.enabled = true;
+    }
+    
+    public bool IsVisibleFromMainCam(Renderer renderer)
+    {
+        Plane[] planes = GeometryUtility.CalculateFrustumPlanes(Camera.main);
+        return GeometryUtility.TestPlanesAABB(planes, renderer.bounds);
+    }
+
+}
