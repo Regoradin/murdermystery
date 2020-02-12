1d69c86db250ffcc73c3a8dca9d096bb45148eb7
Charles Yao <chongbinyao_hz@163.com>
2020-02-11 19:32:12 -0800

    Update .gitignore and integrated nodding

---
+++ Unity/MurderMystery/Assets/ScriptExample/Scripts/Main.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using FrameSynthesis.VR;
+
+namespace ScriptExample
+{
+    public class Main : MonoBehaviour
+    {
+        [SerializeField]
+        ScriptEngine scriptEngine;
+        [SerializeField]
+        AudioSource gestureSound;
+
+        void Start()
+        {
+            VRGestureRecognizer.Current.NodHandler += OnNod;
+            VRGestureRecognizer.Current.HeadshakeHandler += OnHeadshake;
+        }
+
+        void OnNod()
+        {
+            if (scriptEngine.IsWaitingYesNo)
+            {
+                scriptEngine.AnswerYes();
+                gestureSound.Play();
+                Debug.Log("I called the method");
+            }
+        }
+
+        void OnHeadshake()
+        {
+            if (scriptEngine.IsWaitingYesNo)
+            {
+                scriptEngine.AnswerNo();
+                gestureSound.Play();
+            }
+        }
+    }
+}
