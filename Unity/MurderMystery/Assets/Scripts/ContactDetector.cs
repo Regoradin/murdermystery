0379ea2fe3e755711c527b0ea5a03b4802ac080e
Oliver <oliver@theabates.com>
2020-02-13 17:14:15 -0800

    Fixing some things for VR

---
+++ Unity/MurderMystery/Assets/Scripts/ContactDetector.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContactDetector : MonoBehaviour
+{
+
+	public string input;
+	// Start is called before the first frame update
+	private void OnTriggerEnter(Collider other)
+	{
+		StoryStructure.Instance.Interact(input);
+	}
+
+}
