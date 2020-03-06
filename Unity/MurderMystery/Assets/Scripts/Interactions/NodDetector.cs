15256100d235629959e5ad6f9145322b2a166760
Regoradin <oliver@theabates.com>
2020-03-05 18:57:23 -0800

    Character talking interrupts interactions

--- Unity/MurderMystery/Assets/Scripts/Interactions/NodDetector.cs
+++ Unity/MurderMystery/Assets/Scripts/Interactions/NodDetector.cs
@@ -5,53 +5,70 @@ using FrameSynthesis.VR;
 
 public class NodDetector : MonoBehaviour
 {
-	public string yesInteraction;
-	public string noInteraction;
+    public string yesInteraction;
+    public string noInteraction;
 
-	public float cooldown;
-	private float cooldownRemaining;
-	private bool coolingDown = false;
+    public float cooldown;
+    private float cooldownRemaining;
+    private bool coolingDown = false;
     private TakeResponse gaze;
 
-	private void Start()
-	{
+
+    public List<Animator> interruptingAnimators;
+    private bool animatorsAtBase
+    {
+        get
+        {
+            foreach (Animator anim in interruptingAnimators)
+            {
+                if(!anim.GetAnimatorStateInfo.IsName("Base"))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    private void Start()
+    {
         gaze = (TakeResponse)FindObjectOfType(typeof(TakeResponse));
-		cooldownRemaining = cooldown;
-		VRGestureRecognizer.Current.NodHandler += OnNod;
-		VRGestureRecognizer.Current.HeadshakeHandler += OnHeadshake;
-	}
+        cooldownRemaining = cooldown;
+        VRGestureRecognizer.Current.NodHandler += OnNod;
+        VRGestureRecognizer.Current.HeadshakeHandler += OnHeadshake;
+    }
 
-	private void Update()
-	{
-		if (cooldownRemaining <= 0)
-		{
-			coolingDown = false;
-		}
-		else
-		{
-			cooldownRemaining -= Time.deltaTime;
-		}
-	}
+    private void Update()
+    {
+        if (cooldownRemaining <= 0)
+        {
+            coolingDown = false;
+        }
+        else
+        {
+            cooldownRemaining -= Time.deltaTime;
+        }
+    }
 
-	private void OnNod()
-	{
-		if (!coolingDown && gaze.isGazing())
-		{
-			Debug.Log("Nodding detector");
-			StoryStructure.Instance.Interact(yesInteraction);
-			coolingDown = true;
-			cooldownRemaining = cooldown;
-		}
-	}
-	private void OnHeadshake()
-	{
-		if (!coolingDown)
-		{
+    private void OnNod()
+    {
+        if (!coolingDown && gaze.isGazing() && animatorsAtBase)
+        {
+            Debug.Log("Nodding detector");
+            StoryStructure.Instance.Interact(yesInteraction);
+            coolingDown = true;
+            cooldownRemaining = cooldown;
+        }
+    }
+    private void OnHeadshake()
+    {
+        if (!coolingDown && gaze.isGazing && animatorsAtBase)
+        {
             Debug.Log("Headshake detector");
-			StoryStructure.Instance.Interact(noInteraction);
-			coolingDown = true;
-			cooldownRemaining = cooldown;
-		}
-	}
+            StoryStructure.Instance.Interact(noInteraction);
+            coolingDown = true;
+            cooldownRemaining = cooldown;
+        }
+    }
 
 }
